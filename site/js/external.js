cbe898536bc4a3ec501541bc50e4f25dcdaac564
morkett <dcorkett@spartaglobal.co>
2017-02-24 15:57:36 +0000

    added js file and optimised image

--- site/js/external.js
+++ site/js/external.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
-//   $("#js-projects-panel").click(function(){
+//   $(".js-panel-click").click(function(){
 //     event.preventDefault();
-//     $(this).addClass("no-scroll");
-//
-//         if ( $( this ).hasClass( "no-scroll" ) ) {
+//     $("body").addClass("js-body-overflow");
 //                $('html,body').animate({scrollTop:$(this.hash).offset().top}, 700);
-//            }
+//            setTimeout(function () {
+//              $(".js-panel-click").removeClass("js-panel-click");
+//            }, 10);
 //
 // });
+
+
 
 var stickyTop = $('.mdl-tabs__tab-bar').offset().top;
 
 $(window).on( 'scroll', function(){
         if ($(window).scrollTop() >= stickyTop) {
-          console.log("hit");
             $('.mdl-tabs__tab-bar').addClass("js-scroll-fix");
             $(".mdl-tabs__panel ul").addClass("js-scroll-padding");
         } else {
