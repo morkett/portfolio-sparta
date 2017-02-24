b1e7912922a8eef4de0968da7820799ba4d44078
morkett <dcorkett@spartaglobal.co>
2017-02-24 15:16:18 +0000

    site contruction

--- site/js/external.js
+++ site/js/external.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
+//   $("#js-projects-panel").click(function(){
+//     event.preventDefault();
+//     $(this).addClass("no-scroll");
+//
+//         if ( $( this ).hasClass( "no-scroll" ) ) {
+//                $('html,body').animate({scrollTop:$(this.hash).offset().top}, 700);
+//            }
+//
+// });
+
+var stickyTop = $('.mdl-tabs__tab-bar').offset().top;
+
+$(window).on( 'scroll', function(){
+        if ($(window).scrollTop() >= stickyTop) {
+          console.log("hit");
+            $('.mdl-tabs__tab-bar').addClass("js-scroll-fix");
+            $(".mdl-tabs__panel ul").addClass("js-scroll-padding");
+        } else {
+            $('.mdl-tabs__tab-bar').removeClass("js-scroll-fix");
+            $(".mdl-tabs__panel ul").removeClass("js-scroll-padding");
+        }
+    });
+
 
 });
