ca0beb0954fbca13eafa2d05a5f12a9a62c002bc
morkett <dcorkett@spartaglobal.co>
2017-02-25 13:19:17 +0000

    gerneral edits - changed layout

--- site/js/external.js
+++ site/js/external.js
@@ -16,10 +16,13 @@ var stickyTop = $('.mdl-tabs__tab-bar').offset().top;
 $(window).on( 'scroll', function(){
         if ($(window).scrollTop() >= stickyTop) {
             $('.mdl-tabs__tab-bar').addClass("js-scroll-fix");
-            $(".mdl-tabs__panel ul").addClass("js-scroll-padding");
+            $(".tab-padding").addClass("js-scroll-padding");
+            $(".tab-padding-cv").addClass("js-scroll-padding-cv");
         } else {
             $('.mdl-tabs__tab-bar').removeClass("js-scroll-fix");
             $(".mdl-tabs__panel ul").removeClass("js-scroll-padding");
+            $(".tab-padding-cv").removeClass("js-scroll-padding-cv");
+
         }
     });
 
