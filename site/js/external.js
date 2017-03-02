a0f0d428c00a4f8b3b3db8756b430bc11e41484e
morkett <dcorkett@spartaglobal.co>
2017-03-02 14:10:05 +0000

    minor changes

--- site/js/external.js
+++ site/js/external.js
@@ -1,31 +1,40 @@
 $(document).ready(function() {
-//   $("#projects-panel").click(function(){
-//     event.preventDefault();
-//     $("body").addClass("js-body-overflow");
-//                $('html,body').animate({scrollTop:$(this.hash).offset().top - 50}, 700);
-//            setTimeout(function () {
-//              $(".js-panel-click").removeClass("js-panel-click");
-//            }, 10);
-//
-// });
+  //
+  // $('.mdl-layout__container').click(function(){
+  //   console.log('hit');
+  //   // event.preventDefault();
+  //   // $('body').addClass('js-body-overflow');
+  //   // $('html,body').animate({scrollTop: $(this.hash).offset().top - 50}, 700);
+  //
+  //
+  // });
+
+
+$('.js-panel-click').click(function(){
+
+    event.preventDefault();
+    $('html, body').animate({
+    scrollTop: $("#projects-panel").position().top
+}, 500);
+
+});
 
 
 
+  var stickyTop = $('.mdl-tabs__tab-bar').offset().top;
 
-var stickyTop = $('.mdl-tabs__tab-bar').offset().top;
+  $(window).on( 'scroll', function(){
+    if ($(window).scrollTop() >= stickyTop) {
+      $('.mdl-tabs__tab-bar').addClass('js-scroll-fix');
+      $('.tab-padding').addClass('js-scroll-padding');
+      $('.tab-padding-cv').addClass('js-scroll-padding-cv');
+    } else {
+      $('.mdl-tabs__tab-bar').removeClass('js-scroll-fix');
+      $('.mdl-tabs__panel ul').removeClass('js-scroll-padding');
+      $('.tab-padding-cv').removeClass('js-scroll-padding-cv');
 
-$(window).on( 'scroll', function(){
-        if ($(window).scrollTop() >= stickyTop) {
-            $('.mdl-tabs__tab-bar').addClass("js-scroll-fix");
-            $(".tab-padding").addClass("js-scroll-padding");
-            $(".tab-padding-cv").addClass("js-scroll-padding-cv");
-        } else {
-            $('.mdl-tabs__tab-bar').removeClass("js-scroll-fix");
-            $(".mdl-tabs__panel ul").removeClass("js-scroll-padding");
-            $(".tab-padding-cv").removeClass("js-scroll-padding-cv");
-
-        }
-    });
+    }
+  });
 
 
 });
